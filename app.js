b598af5d201f637a37a7827f4f7fdaaa26a0b45c
codementee <codementee@outlook.com>
2020-10-18 14:27:26 +0500

    finished

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 // hamburger menu 
-
+const boddy = document.querySelector('body');
 const hamburger = document.querySelectorAll('.toggle__btn')[0];
-const close = document.querySelectorAll('.toggle__btn')[1];
+const closeBtn = document.querySelectorAll('.toggle__btn')[1];
 const navigation = document.querySelector('.header__nav .nav__ul');
+const overlay = document.querySelector('.overlay');
 
 hamburger.addEventListener('click', ()=>{
+    boddy.classList.toggle('no-scroll');
     navigation.classList.toggle('show');
     navigation.classList.remove('hide')
     hamburger.classList.toggle('hide');
-    close.classList.toggle('hide');
+    closeBtn.classList.toggle('hide');
+    overlay.classList.toggle('hide');
 })
 
-close.addEventListener('click', ()=>{
+closeBtn.addEventListener('click', ()=>{
+    boddy.classList.toggle('no-scroll');
     navigation.classList.toggle('hide')
     navigation.classList.remove('show')
     hamburger.classList.toggle('hide')
-    close.classList.toggle('hide')
+    closeBtn.classList.toggle('hide')
+    overlay.classList.toggle('hide')
 })
+
+
